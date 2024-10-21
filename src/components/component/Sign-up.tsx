c9f206000cd9ab45179dd4a747568d1ccf3532e1
MalikaLaouiti <laouiti.malika@yahoo.com>
2024-10-21 23:23:15 +0200

    updated sign up : form shadcn, toast, control de saisie

--- src/components/component/Sign-up.tsx
+++ src/components/component/Sign-up.tsx
@@ -1,101 +1,173 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { createUser } from "@/action/employee";
-import { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 export default function Login() {
-  const [formData, setFormData] = useState({
-    cin: '',
-    password: '',
-    name: '',
-    gender: '',
-    phone_number: '',
-    email: '',
-    grade: ''
+  const form = useForm({
+    defaultValues: {
+      cin: '',
+      password: '',
+      name: '',
+      gender: '',
+      phone_number: '',
+      email: '',
+      grade: ''
+    },
+    mode: 'onBlur',
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault(); // Prevent default form submission
-
-    // Create a FormData instance
+  const onSubmit = async (data: Record<string, string>) => {
     const formDataToSubmit = new FormData();
-
-    // Append each field from formData to formDataToSubmit
-    Object.entries(formData).forEach(([key, value]) => {
+    Object.entries(data).forEach(([key, value]) => {
       formDataToSubmit.append(key, value);
     });
+    try {
+      await createUser(formDataToSubmit); // Send FormData to createUser
+      toast.success("Utilisateur créé avec succès !");
+    } catch (error) {
+      console.error("Failed to create user:", error);
+      toast.error("Échec de la création de l'utilisateur.");
+    }
 
-    await createUser(formDataToSubmit); // Send FormData to createUser
   };
 
   return (
     <div className="max-w-md mx-auto mt-10 mb-10">
-      <form onSubmit={handleSubmit} className="space-y-6 bg-white p-8 rounded-xl shadow-md">
-        <h2 className="text-2xl font-bold text-center mb-6">Inscription à RH Application</h2>
-        <div className="space-y-2">
-          <Label htmlFor="cin">CIN</Label>
-          <Input id="cin" name="cin" required value={formData.cin} onChange={handleInputChange} />
-        </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 bg-white p-8 rounded-xl shadow-md">
+          <h2 className="text-2xl font-bold text-center mb-6">Inscription à RH Application</h2>
 
-        <div className="space-y-2">
-          <Label htmlFor="password">Mot de passe</Label>
-          <Input id="password" name="password" type="password" required value={formData.password} onChange={handleInputChange} />
-        </div>
+          <FormField
+            control={form.control}
+            name="cin"
+            rules={{ required: "Entrer votre CIN", pattern: { value: /^\d{8}$/, message: "Entrer correctement votre CIN (8 nombres) " } }}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>CIN</FormLabel>
+                <FormControl>
+                  <Input placeholder="Entrer CIN" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <div className="space-y-2">
-          <Label htmlFor="name">Nom</Label>
-          <Input id="name" name="name" required value={formData.name} onChange={handleInputChange} />
-        </div>
+          <FormField
+            control={form.control}
+            name="password"
+            rules={{ required: "Entrer votre Mot de passe", maxLength: { value: 8, message: "Mot de passe doit etre moin que 8 caracteres" }, minLength: { value: 5, message: "Mot de passe doit etre plus que 5 caracteres" }, pattern: { value: /^[A-Z0-9._%+*/!?-]+/, message: "Entrer mot de passse fort" } }}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Mot de passe</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="Entrer Password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <div className="space-y-2">
-          <Label htmlFor="gender">Genre</Label>
-          <select
-            id="gender"
+          <FormField
+            control={form.control}
+            name="name"
+            rules={{ required: "Entrer votre nom et prenom" }}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nom</FormLabel>
+                <FormControl>
+                  <Input placeholder="Entrer nom et prenom" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
             name="gender"
-            required
-            value={formData.gender}
-            onChange={handleInputChange}
-            className="block w-full border border-gray-300 rounded-md p-2 focus:ring-blue-500 focus:border-blue-500"
-          >
-            <option value="Homme">Homme</option>
-            <option value="Femme">Femme</option>
-          </select>
-        </div>
+            rules={{ required: "Selectionner votre sex" }}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Genre</FormLabel>
+                <FormControl>
+                  <select {...field} className="block w-full border border-gray-300 rounded-md p-2 focus:ring-blue-500 focus:border-blue-500">
+                    <option value="">Selectionner votre sex</option>
+                    <option value="Homme">Homme</option>
+                    <option value="Femme">Femme</option>
+                  </select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <div className="space-y-2">
-          <Label htmlFor="phone_number">Numéro de téléphone</Label>
-          <Input id="phone_number" name="phone_number" type="tel" required value={formData.phone_number} onChange={handleInputChange} />
-        </div>
+          <FormField
+            control={form.control}
+            name="phone_number"
+            rules={{ required: "Entrer votre numero de telephone", pattern: { value: /^\d{8}$/, message: "Entrer un correct numero" } }}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Numéro de téléphone</FormLabel>
+                <FormControl>
+                  <Input type="tel" placeholder="Entrer un numero de telephone" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <div className="space-y-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" name="email" type="email" required value={formData.email} onChange={handleInputChange} />
-        </div>
+          <FormField
+            control={form.control}
+            name="email"
+            rules={{ required: "Entrer un E-mail", pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: "Entrer un correct E-mail" } }}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" placeholder="Entrer un E-mail" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <div className="space-y-2">
-          <Label htmlFor="grade">Sélectionnez votre rôle</Label>
-          <select
-            id="grade"
+          <FormField
+            control={form.control}
             name="grade"
-            required
-            value={formData.grade}
-            onChange={handleInputChange}
-            className="block w-full border border-gray-300 rounded-md p-2 focus:ring-blue-500 focus:border-blue-500"
-          >
-            <option value="Admin">Admin</option>
-            <option value="Employee">Employee</option>
-          </select>
-        </div>
-
-        <Button type="submit" className="w-full">S'inscrire</Button>
-      </form>
+            rules={{ required: "Selectionner votre role" }}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Sélectionnez votre rôle</FormLabel>
+                <FormControl>
+                  <select {...field} className="block w-full border border-gray-300 rounded-md p-2 focus:ring-blue-500 focus:border-blue-500">
+                    <option value="">Sélectionnez votre rôle</option>
+                    <option value="Admin">Admin</option>
+                    <option value="Employee">Employee</option>
+                  </select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full">S'inscrire</Button>
+          <ToastContainer/>
+        </form>
+      </Form>
     </div>
   );
 }
